8c28608b681c2adfb06f29510065cc7ffaba34b5
Ricky428 <103875348+Ricky428@users.noreply.github.com>
2023-05-02 13:02:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 3D-button
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-mkp9bc)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="center">
+      <input type="checkbox" name="" id="" />
+      <span>On</span>
+      <span>Off</span>
+    </div>
+  );
+}
